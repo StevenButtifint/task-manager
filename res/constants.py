e91c5d25edf4d2fe652315d628723656e6cbb8b8
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-06-13 15:24:09 +0100

    added colour constants

--- res/constants.py
+++ res/constants.py
@@ -2,3 +2,10 @@
 LOCAL_DB_DIR = 'res/local_db.db'
 UI_FILE_DIR = 'res/ui_main.ui'
 
+# colours
+DAY_BAR_ACTIVE = '#72eb3a'
+DAY_BAR_INACTIVE = '#353535'
+
+TASK_TEXT_COMPLETED = '#365a08'
+TASK_TEXT_INCOMPLETE = 'white'
+
