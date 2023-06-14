c3f2e9ba2da18c014f26cd2fd7b36f20eaaad223
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-06-14 11:20:41 +0100

    added search table method

--- res/database.py
+++ res/database.py
@@ -6,3 +6,8 @@ class Database:
     def __init__(self):
         self.db_connection = sqlite3.connect(LOCAL_DB_DIR)
         self.db_cursor = self.db_connection.cursor()
+
+    def search_table(self, table_name, field, value):
+        self.db_cursor.execute(f"SELECT * FROM {table_name} WHERE {field} = ?", (value,))
+        return self.db_cursor.fetchall()
+
