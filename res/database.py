f4700d0ace2237fbbb5d06f77e30f1a7e8411df5
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-06-14 11:20:56 +0100

    added get weekday task method

--- res/database.py
+++ res/database.py
@@ -7,6 +7,9 @@ class Database:
         self.db_connection = sqlite3.connect(LOCAL_DB_DIR)
         self.db_cursor = self.db_connection.cursor()
 
+    def get_weekday_tasks(self, day):
+        return self.search_table(WEEKDAY_TABLE_NAME, day, 1)
+
     def search_table(self, table_name, field, value):
         self.db_cursor.execute(f"SELECT * FROM {table_name} WHERE {field} = ?", (value,))
         return self.db_cursor.fetchall()
