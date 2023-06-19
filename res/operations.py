a869800bcb687eab88b8cb21444dee5ec8e8911d
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-06-19 19:18:02 +0100

    added get day difference function

--- res/operations.py
+++ res/operations.py
@@ -8,3 +8,13 @@ def get_today_date():
 def get_weekday_index():
     dt = datetime.now()
     return dt.weekday()
+
+
+def get_day_difference(first_date, second_date):
+    if first_date == DATE_BLANK:
+        return 0
+    else:
+        d1 = datetime.strptime(first_date, "%Y/%m/%d")
+        d2 = datetime.strptime(second_date, "%Y/%m/%d")
+        delta = d2 - d1
+        return delta.days
