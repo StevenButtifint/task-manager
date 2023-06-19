542a5552e2d62721e5b61409e4b0c19db5c10cb2
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-06-19 19:21:06 +0100

    added table name constants

--- res/constants.py
+++ res/constants.py
@@ -9,3 +9,8 @@ DAY_BAR_INACTIVE = '#353535'
 TASK_TEXT_COMPLETED = '#365a08'
 TASK_TEXT_INCOMPLETE = 'white'
 
+# database tables
+WEEKDAY_TABLE_NAME = "weekday_tasks"
+DATES_TABLE_NAME = "dates_tasks"
+CYCLE_TABLE_NAME = "cycle_tasks"
+
