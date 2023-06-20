1d400d72e060b81ac9d923e7f5619eff050b7e66
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-06-20 22:30:49 +0100

    added clear new weekday task entry method

--- app.py
+++ app.py
@@ -15,6 +15,21 @@ class MainWindow(QMainWindow):
         self.local_db = Database()
 
 
+    def clear_new_weekday_task_entry(self, title_entry, description_entry, rollover_slider):
+        for day_checkbox in WEEKDAY_CHECKBOXES:
+            checkbox = self.findChild(QCheckBox, day_checkbox)
+            checkbox.setChecked(False)
+
+        for day_tile in WEEKDAY_NEW_TASK_TILES:
+            tile = self.findChild(QFrame, day_tile)
+            tile.setStyleSheet(NEW_WEEKDAY_TASK_TILE_UNCHECKED)
+
+        title_entry.clear()
+        description_entry.clear()
+        rollover_slider.setValue(0)
+        self.change_rollover_lbl()
+
+
 if __name__ == "__main__":
     app = QApplication([])
     window = MainWindow()
