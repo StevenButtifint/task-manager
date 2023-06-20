adb1719767deca7d49a1d6af221e8789d3503295
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-06-20 22:31:04 +0100

    added add weekday task method

--- app.py
+++ app.py
@@ -15,6 +15,21 @@ class MainWindow(QMainWindow):
         self.local_db = Database()
 
 
+    def add_weekday_task(self):
+        task_title_qle = self.findChild(QLineEdit, 'led_weekday_title')
+        task_description_pte = self.findChild(QPlainTextEdit, 'pte_weekday_desc')
+        rollover_slider = self.findChild(QSlider, 'sld_weekday_rollover')
+
+        day_checkboxes_bool = []
+        for day_checkbox in WEEKDAY_CHECKBOXES:
+            checkbox = self.findChild(QCheckBox, day_checkbox)
+            day_checkboxes_bool.append(int(checkbox.isChecked()))
+            checkbox.setChecked(False)
+        self.local_db.add_weekday_task(task_title_qle.text(), task_description_pte.toPlainText(), *day_checkboxes_bool, rollover_slider.value())
+        self.refresh_today_tasks()
+
+        self.clear_new_weekday_task_entry(task_title_qle, task_description_pte, rollover_slider)
+
     def clear_new_weekday_task_entry(self, title_entry, description_entry, rollover_slider):
         for day_checkbox in WEEKDAY_CHECKBOXES:
             checkbox = self.findChild(QCheckBox, day_checkbox)
